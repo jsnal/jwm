80ad2320d5ff2bbf2950d7c69b08e75a65118fe5
jsnal <jasonlongball@gmail.com>
2020-06-29 20:10:54 -0400

    feat: working frame drawing and window creation
    The start of a window lifecycle is its creation and framing.

--- src/window_manager.cc
+++ src/window_manager.cc
@@ -46,6 +46,31 @@ void WindowManager::Run()
 
   XSetErrorHandler(&WindowManager::OnXError);
 
+  XGrabServer(display_);
+
+  Window rRoot, rParent;
+  Window* topLevelWindows;
+  unsigned int numTopLevelWindows;
+
+  CHECK(XQueryTree(
+        display_,
+        root_,
+        &rRoot,
+        &rParent,
+        &topLevelWindows,
+        &numTopLevelWindows
+        ));
+
+  CHECK_EQ(rRoot, root_);
+
+  for (unsigned int i = 0; i < numTopLevelWindows; i++)
+  {
+    Frame(topLevelWindows[i], true);
+  }
+
+  XFree(topLevelWindows);
+  XUngrabServer(display_);
+
   for (;;)
   {
     XEvent e;
@@ -53,6 +78,12 @@ void WindowManager::Run()
 
     switch (e.type)
     {
+      case ConfigureRequest:
+        OnConfigureRequest(e.xconfigurerequest);
+        break;
+      case MapRequest:
+        OnMapRequest(e.xmaprequest);
+        break;
       case CreateNotify:
         OnCreateNotify(e.xcreatewindow);
         break;
@@ -79,6 +110,77 @@ int WindowManager::OnXError(Display* display, XErrorEvent* e)
   return 0;
 }
 
+
+/*
+ * Configure a window with important default parameters. Since the window is
+ * invisible the window manager can grant requests directly without direct
+ * modifitcation by invoking this function.
+ */
+
+void WindowManager::OnConfigureRequest(const XConfigureRequestEvent& e)
+{
+  XWindowChanges changes;
+
+  changes.x = e.x;
+  changes.y = e.y;
+  changes.width = e.width;
+  changes.height = e.height;
+  changes.border_width = e.border_width;
+  changes.sibling = e.above;
+
+  XConfigureWindow(display_, e.window, e.value_mask, &changes);
+}
+
+void WindowManager::OnMapRequest(const XMapRequestEvent& e)
+{
+  Frame(e.window, false);
+  XMapWindow(display_, e.window);
+}
+
 void WindowManager::OnCreateNotify(const XCreateWindowEvent& e) {}
 void WindowManager::OnDestroyNotify(const XDestroyWindowEvent& e) {}
 void WindowManager::OnReparentNotify(const XReparentEvent& e) {}
+
+void WindowManager::Frame(Window w, bool isPrexisting)
+{
+  const unsigned int BORDER_WIDTH = 3;
+  const unsigned long BORDER_COLOR = 0xff0000;
+  const unsigned long BG_COLOR = 0x0000ff;
+
+  XWindowAttributes windowAttributes;
+  CHECK(XGetWindowAttributes(display_, w, &windowAttributes));
+
+  if (isPrexisting)
+    if (windowAttributes.override_redirect
+        || windowAttributes.map_state != IsViewable)
+      return;
+
+  const Window frame = XCreateSimpleWindow(
+      display_,
+      root_,
+      windowAttributes.x,
+      windowAttributes.y,
+      windowAttributes.width,
+      windowAttributes.height,
+      BORDER_WIDTH,
+      BORDER_COLOR,
+      BG_COLOR
+      );
+
+  XSelectInput(
+      display_,
+      frame,
+      SubstructureRedirectMask | SubstructureNotifyMask
+      );
+
+  XAddToSaveSet(display_, w);
+
+  XReparentWindow(
+      display_,
+      w,
+      frame,
+      0, 0
+      );
+
+  XMapWindow(display_, frame);
+}
--- src/window_manager.hh
+++ src/window_manager.hh
@@ -18,7 +18,12 @@ class WindowManager {
     static int OnWMDetected(Display* display, XErrorEvent* e);
     static bool wm_detected_;
 
+    void OnConfigureRequest(const XConfigureRequestEvent& e);
+    void OnMapRequest(const XMapRequestEvent& e);
     void OnCreateNotify(const XCreateWindowEvent& e);
     void OnDestroyNotify(const XDestroyWindowEvent& e);
     void OnReparentNotify(const XReparentEvent& e);
+
+    /* Map Helpers */
+    void Frame(Window w, bool isPrexisting);
 };
